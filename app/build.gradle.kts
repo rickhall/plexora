bc9633cf3a146835b07dea39d9bcf93b1f69f300
Richard S. Hall <heavy@ungoverned.org>
2026-06-04 16:15:30 -0400

    Change API level.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "org.ungoverned.plexora"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "org.ungoverned.plexora"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 2
         versionName = "1.0"
 
